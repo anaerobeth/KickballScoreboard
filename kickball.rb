8aa1e8f89209e3ed2e071bacfff3eecb7a4d82fc
Beth Tenorio <betsky98@gmail.com>
2013-08-28 15:42:58 -0400

    version with hashes and array inputs

--- kickball.rb
+++ kickball.rb
@@ -1,6 +1,7 @@
 require 'pry'
 # set state at the start
 game_over = false
+game_number = 0
 
 # begin entering team names and scores
 while game_over == false
@@ -13,13 +14,16 @@ while game_over == false
   team_2_score = ''
 
 
+
   # target data structure
   # games = [
+  #   1 =>
   #   {'team_1_name' => 'Sick Kicks',
   #    'team_1_score' => 2,
   #    'team_2_name' => 'Good Will Bunting',
   #    'team_2_score' => 3,
   #   },
+  #   2 =>
   #   {'team_1_name' => 'Sick Kicks',
   #    'team_1_score' => 5,
   #    'team_2_name' => 'Angels in the Outfield',
@@ -28,7 +32,6 @@ while game_over == false
   # ]
 
 
-  # capture the names and scores into a hash
   # team 1's name and their score
   while team_1_name == ''
     puts "What was team 1's name?"
@@ -58,7 +61,12 @@ while game_over == false
     team_2_score = gets.chomp.to_i
   end
 
+  # capture the names and scores into a hash
 
+  names_and_scores[game_number] = {'team_1_name' => team_1_name, 'team_1_score' => team_1_score, 'team_2_name' => team_2_name, 'team_2_score' => team_2_score,  }
+
+  # capture the game stats into an array
+  games << names_and_scores
 
   # provide option to continue entering team names and scores
   input = ''
@@ -69,23 +77,28 @@ while game_over == false
 
   if input.downcase == 'y'
     game_over = false
+    game_number += 1
   elsif input == 'n'
     game_over = true
-    puts "Bye!"
+"
     # exit loop
     break
   end
+end
 
   # extract a method with 4 arguments
   def identify_winner(team_1_name, team_2_name, team_1_score, team_2_score)
     return team_1_score > team_2_score
   end
 
+
+  puts games
+
   #output the winner
   if identify_winner(team_1_name, team_2_name, team_1_score, team_2_score)  == true
-    puts "#{team_1_name} is the victor!"
+    puts "In game #{}, #{team_1_name} is the victor!"
   else
-    puts "#{team_2_name} is the victor!"
+    puts "In game #{}, #{team_2_name} is the victor!"
   end
-end
+
 
