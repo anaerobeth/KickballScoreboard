25d1ff18074e9eec327576aa4b440a822a96366c
Beth Tenorio <betsky98@gmail.com>
2013-08-28 13:22:10 -0400

    Initial commit

---
+++ kickball.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+team_1_name = ''
+team_2_name = ''
+
+#team 1's name and their score
+while team_1_name = ''
+  puts "What was team 1's name?"
+  puts "> "
+  input = gets.chomp
+  team_1_name = input
+end
+
+puts "Team 1 name is: #{team_1_name}"
+
+while team_1_name == team_2_name
+
+
+#team 2's name and their score
+  #check that team names are unique
+
+#output the winner
