dd0b9d12a0381c7915f35e76ec6c881aeeb1785d
Beth Tenorio <betsky98@gmail.com>
2013-08-28 14:48:23 -0400

    Add target data structure

--- kickball.rb
+++ kickball.rb
@@ -1,16 +1,35 @@
 require 'pry'
-#set game state at the start
+# set state at the start
 game_over = false
 
-#enter game loop
+# begin entering team names and scores
 while game_over == false
+  # define array, hash and variables to be used
+  names_and_scores = {}
+  games = []
   team_1_name = ''
   team_2_name = ''
   team_1_score= ''
   team_2_score = ''
 
 
-  #team 1's name and their score
+  # target data structure
+  # games = [
+  #   {'team_1_name' => 'Sick Kicks',
+  #    'team_1_score' => 2,
+  #    'team_2_name' => 'Good Will Bunting',
+  #    'team_2_score' => 3,
+  #   },
+  #   {'team_1_name' => 'Sick Kicks',
+  #    'team_1_score' => 5,
+  #    'team_2_name' => 'Angels in the Outfield',
+  #    'team_2_score' => 3,
+  #   }
+  # ]
+
+
+  # capture the names and scores into a hash
+  # team 1's name and their score
   while team_1_name == ''
     puts "What was team 1's name?"
     print "> "
@@ -24,8 +43,9 @@ while game_over == false
   end
 
 
-  #team 2's name and their score
-    #check that team names are unique
+
+  # team 2's name and their score
+    # check that team names are unique
   while team_2_name == '' || team_1_name == team_2_name
     puts "What was team 2's name?"
     print "> "
@@ -38,19 +58,9 @@ while game_over == false
     team_2_score = gets.chomp.to_i
   end
 
-  #extract a method with 4 arguments
-  def identify_winner(team_1_name, team_2_name, team_1_score, team_2_score)
-    return team_1_score > team_2_score
-  end
 
-  #output the winner
-  if identify_winner(team_1_name, team_2_name, team_1_score, team_2_score)  == true
-    puts "#{team_1_name} is the victor!"
-  else
-    puts "#{team_2_name} is the victor!"
-  end
 
-  # provide option to continue
+  # provide option to continue entering team names and scores
   input = ''
   while input == ''
      puts "Would you like to provide another game? (Y/N)"
@@ -62,9 +72,20 @@ while game_over == false
   elsif input == 'n'
     game_over = true
     puts "Bye!"
-    # exit game loop
+    # exit loop
     break
   end
 
+  # extract a method with 4 arguments
+  def identify_winner(team_1_name, team_2_name, team_1_score, team_2_score)
+    return team_1_score > team_2_score
+  end
+
+  #output the winner
+  if identify_winner(team_1_name, team_2_name, team_1_score, team_2_score)  == true
+    puts "#{team_1_name} is the victor!"
+  else
+    puts "#{team_2_name} is the victor!"
+  end
 end
 
